9e67dd66abc97e51be2aa61e231bb9a9617cea42
ClintFix <clint.fix@gmail.com>
2021-04-05 13:48:14 -0600

    get all users api endpoint working

--- api/server.js
+++ api/server.js
@@ -5,4 +5,25 @@ const Users = require('./users/model')
 const server = express();
 server.use(express.json());
 
+// ENDPOINTS
+
+// [GET] - /api/users - get all users
+server.get("/api/users", (req, res) => {
+    Users.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({message: err.message})
+        })
+})
+
+// [GET] - /api/users/:id - get user by id
+
+// [POST] - /api/users - create new user
+
+// [PUT] - /api/users/:id - update user and return modified user
+
+// [DELETE] - /api/users/:id - remove user by id and return deleted user
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
